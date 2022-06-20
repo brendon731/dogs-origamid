6c777db0befe7670b009edd480d33bb4960acf9c
unknown <brendon_gh@hotmail.com>
2022-06-20 15:21:10 -0300

    footer

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -2,7 +2,7 @@ import {ReactComponent as Dogs} from "./../Assets/dogs.svg"
 import style from "./footer.module.css"
 export default function Footer(){
     return(
-        <footer>
+        <footer className={style.footer}>
             <Dogs/>
             <p>Dogs. Alguns direitos reservados</p>
         </footer>
